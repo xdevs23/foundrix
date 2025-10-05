bed1ee46457f7d61858418a6380b673038f22f9b
Simão Gomes Viana <devel@superboring.dev>
2025-10-06 01:26:01 +0200

    packages: add git-aliases and pickrange

---
+++ packages/git-aliases.nix
@@ -0,0 +1,8 @@
+{ pkgs, ... }: pkgs.symlinkJoin {
+  name = "git-aliases";
+  paths = [
+    (pkgs.writeShellScriptBin "gpick" ''
+      exec git cherry-pick -s "$@"
+    '')
+  ];
+}
---
+++ packages/pickrange.nix
@@ -0,0 +1,13 @@
+{ pkgs, ... }: pkgs.symlinkJoin {
+  name = "pickrange";
+  paths = [
+    (pkgs.writeShellScriptBin "pickrange" ''
+      git rev-list --reverse --topo-order $1^..$2 | while read rev
+      do echo "$rev"; done | xargs git cherry-pick -s
+    '')
+    (pkgs.writeShellScriptBin "pickrangeib" ''
+      git rev-list --reverse --topo-order $1^..$2 | while read rev
+      do git cherry-pick -s $rev || git cherry-pick --skip; done
+    '')
+  ];
+}
