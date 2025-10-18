081c2a0b4fdaa345fdf4c5d61c5338285f0059ce
Simão Gomes Viana <devel@superboring.dev>
2025-10-19 01:14:41 +0200

    hardware: gpu: amd: add more rocm stuff

--- hardware/gpu/amd.nix
+++ hardware/gpu/amd.nix
@@ -33,6 +33,32 @@
         else
           pkgs.rocmPackages;
     };
+    rocmScript = {
+      name = "Generate a shell script package that prepares your script for proper ROCm usage";
+      type = lib.types.package;
+      readOnly = true;
+      default = name: script: pkgs.writeShellScriptBin name ''
+        gfxver="$(${lib.getExe' (namespacedCfg __curPos).rocmPackages.rocminfo "rocminfo"} | \
+          grep 'Name' | grep 'gfx' | head -n1 | ${lib.getExe pkgs.gawk} '{ print $2 }')"
+        version_digits=''${gfxver#gfx}
+        num_digits=''${#version_digits}
+        if [ "$num_digits" -eq 4 ]; then
+          major=''${version_digits:0:2}
+          minor=''${version_digits:2:1}
+          patch=''${version_digits:3:1}
+        elif [ "$num_digits" -eq 3 ]; then
+          major=''${version_digits:0:1}
+          minor=''${version_digits:1:1}
+          patch=''${version_digits:2:1}
+        fi
+        if [ "$version_digits" == "1103" ]; then
+          # 1103 is not supported, it just does not appear in the list, let's use 1102 instead
+          patch=2
+        fi
+        export HSA_OVERRIDE_GFX_VERSION="$major.$minor.$patch"
+        ${script}
+      '';
+    };
   };
 
   config =
@@ -55,6 +81,8 @@
     in
     lib.mkMerge [
       {
+        nixpkgs.config.rocmSupport = cfg.isSupported;
+        nixpkgs.config.rocmPackages = cfg.rocmPackages;
         boot.kernelParams = lib.optional cfg.overclocking.unlock "amdgpu.ppfeaturemask=0xfff7ffff";
 
         environment.systemPackages =
