dea7fa22b6b95cdb2dc6f2d11ab97893842213ae
Simão Gomes Viana <devel@superboring.dev>
2025-10-26 00:21:00 +0200

    config: basic-user-system: add usbutils and pciutils

--- config/basic-user-system.nix
+++ config/basic-user-system.nix
@@ -1,9 +1,9 @@
 { pkgs, ... }: {
   environment = {
     systemPackages = with pkgs; [
-      vim dust ripgrep exfatprogs strace wget curl
+      vim dust ripgrep exfatprogs strace wget curl pciutils
       bash coreutils jq pv socat git dig unzip file tree e2fsprogs
-      bc rsync inetutils zip openssh ddrescue gawk less
+      bc rsync inetutils zip openssh ddrescue gawk less usbutils
     ];
     defaultPackages = [ ];
     variables = {
