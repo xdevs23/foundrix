48f7cf79c165507bd2800527614c4fb9f85b12ab
Simão Gomes Viana <devel@superboring.dev>
2025-10-26 00:24:30 +0200

    config: basic-user-system: nix fmt

--- config/basic-user-system.nix
+++ config/basic-user-system.nix
@@ -1,9 +1,35 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   environment = {
     systemPackages = with pkgs; [
-      vim dust ripgrep exfatprogs strace wget curl pciutils
-      bash coreutils jq pv socat git dig unzip file tree e2fsprogs
-      bc rsync inetutils zip openssh ddrescue gawk less usbutils
+      vim
+      dust
+      ripgrep
+      exfatprogs
+      strace
+      wget
+      curl
+      pciutils
+      bash
+      coreutils
+      jq
+      pv
+      socat
+      git
+      dig
+      unzip
+      file
+      tree
+      e2fsprogs
+      bc
+      rsync
+      inetutils
+      zip
+      openssh
+      ddrescue
+      gawk
+      less
+      usbutils
     ];
     defaultPackages = [ ];
     variables = {
