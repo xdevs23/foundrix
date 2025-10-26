de99d08140ac36cf71eb2c8ba225bc16107b8701
Simão Gomes Viana <devel@superboring.dev>
2025-10-26 23:59:07 +0100

    config: adb: fix

--- config/adb.nix
+++ config/adb.nix
@@ -1,5 +1,5 @@
 { pkgs, lib, ... }:
 {
   programs.adb.enable = true;
-  services.udev.packages = lib.mkIf (lib.versionOlder lib.version "25.10") pkgs.android-udev-rules;
+  services.udev.packages = lib.mkIf (lib.versionOlder lib.version "25.10") [ pkgs.android-udev-rules ];
 }
